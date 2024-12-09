69049bcd9682c1899e1530a8b9788a9d64b3592f
Tarikul Islam Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2024-12-09 12:36:46 +0600

    Add search layout and page components

---
+++ app/search/page.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@/components/layout/box';
+import { Flex } from '@/components/layout/flex';
+
+const SearchPage = () => {
+  return (
+    <Flex justify='center' align='center' mt={4}>
+      <div>Search</div>
+    </Flex>
+  );
+};
+
+export default SearchPage;
